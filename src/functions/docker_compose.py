5f02beba6c624079ee380012ee08b0dc935fefef
James Maple <jmaple@P-WS-01.jmhomelab.local>
2025-09-13 16:43:04 +0100

    Release v1.3

--- src/functions/docker_compose.py
+++ src/functions/docker_compose.py
@@ -26,21 +26,31 @@ def docker_compose_pull(COMPOSE_FILE):
 
 
 
+
 def docker_compose_recreate(COMPOSE_FILE):
     print("\n")
     script.info_msg("Attempting to bring up compose file " + str(COMPOSE_FILE) + " ...")
     print("\n")
 
     try:
-        compose_recreate_cmd = "docker compose -f " + COMPOSE_FILE + " up -d --remove-orphans"
+        # Extract the project name from the filename (without extension)
+        project_name = os.path.splitext(os.path.basename(COMPOSE_FILE))[0]
+
+        # Include -p to set the project name dynamically
+        compose_recreate_cmd = f"docker compose -p {project_name} -f {COMPOSE_FILE} up -d"
+        # --remove-orphans" - Remove orphans breaks multi compose file mode
+        # -p added so portainer recognises each yml as a stack
+
         os.system(compose_recreate_cmd)
         docker_compose_pulled = True
-    except:
+    except Exception as e:
         docker_compose_pulled = False
+        script.err_msg(f"Exception: {e}")
     
     print("\n\n")
-    if docker_compose_pulled == True:
+    if docker_compose_pulled:
         script.ok_msg("Docker Compose recreate OK! - Containers should be up.")
-    elif docker_compose_pulled == False:
+    else:
         script.err_msg("Docker Compose recreate Failed! - Containers may not be up!")
+
 
